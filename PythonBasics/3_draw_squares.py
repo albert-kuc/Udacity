a484c7d2c878bed68c951099eaaa2270e94887ce
Albert <albert.kuc@gmail.com>
2018-07-12 22:43:32 +0100

    feat: Added draw_circle method
    Added new method with new turtle which draws a circle; moved initialise
    window out of draw_square method to main program before calling first
    method

--- PythonBasics/3_draw_squares.py
+++ PythonBasics/3_draw_squares.py
@@ -1,22 +1,43 @@
 import turtle
 
+
 def draw_square():
     """ Method to draw a square """
 
-    """ Initialise window for output"""
-    window = turtle.Screen()
-    window.bgcolor("orange")
-
-    """ Initialise turtle """
+    """ Initialise turtle and customize """
     mikele = turtle.Turtle()
+    mikele.shape("turtle")
+    mikele.color('green')
+    mikele.speed(2)
 
     """ Draw square """
     for i in range(4):
         mikele.forward(100)
         mikele.right(90)
 
-    """ Close window at onclick """
-    window.exitonclick()
 
 
+
+def draw_circle():
+    """ Method to draw a circle """
+
+    """ Initialise turtle and customize """
+    milena = turtle.Turtle()
+    milena.shape("arrow")
+    milena.color('blue')
+    milena.speed(2)
+
+    """ Draw a circle """
+    milena.circle(100)
+
+
+""" Initialise window for output"""
+window = turtle.Screen()
+window.bgcolor("orange")
+
+""" Call the methods """
 draw_square()
+draw_circle()
+
+""" Close window at onclick """
+window.exitonclick()
