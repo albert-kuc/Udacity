dc08802fc9b78071b7052e92ac7a2d4f636300d5
Albert <albert.kuc@gmail.com>
2018-07-12 23:51:21 +0100

    feat: Added draw_art to create a circle out of squares
    Added: new method draw_art, moved initialise turtle from draw_square to
    draw_art, in draw_art loop through draw_square and rotate, added method
    to draw triangle

--- PythonBasics/3_draw_squares.py
+++ PythonBasics/3_draw_squares.py
@@ -1,21 +1,13 @@
 import turtle
 
 
-def draw_square():
+def draw_square(some_turtle):
     """ Method to draw a square """
-
-    """ Initialise turtle and customize """
-    mikele = turtle.Turtle()
-    mikele.shape("turtle")
-    mikele.color('green')
-    mikele.speed(2)
 
     """ Draw square """
     for i in range(4):
-        mikele.forward(100)
-        mikele.right(90)
-
-
+        some_turtle.forward(100)
+        some_turtle.right(90)
 
 
 def draw_circle():
@@ -31,13 +23,42 @@ def draw_circle():
     milena.circle(100)
 
 
+def draw_triangle():
+    """ Method to draw a triangle """
+
+    """ Initialise turtle and customize """
+    mario = turtle.Turtle()
+    mario.shape("square")
+    mario.color('red')
+    mario.speed(2)
+
+    """ Draw a triangle """
+    for i in range(3):
+        mario.right(120)
+        mario.forward(150)
+
+
+def draw_art():
+    """ Initialise turtle and customize """
+    mikele = turtle.Turtle()
+    mikele.shape("turtle")
+    mikele.color('green')
+    mikele.speed(48)
+
+    for i in range(36):
+        draw_square(mikele)
+        mikele.right(10)
+
+
 """ Initialise window for output"""
 window = turtle.Screen()
 window.bgcolor("orange")
 
 """ Call the methods """
-draw_square()
-draw_circle()
+# draw_square()
+# draw_circle()
+# draw_triangle()
+draw_art()
 
 """ Close window at onclick """
 window.exitonclick()
